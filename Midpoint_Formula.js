f5be88936d0e75b7f50610986750cb9ad328597c
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-07-07 14:24:32 +0530

    Add midpoint_formula

---
+++ Midpoint_Formula.js
@@ -0,0 +1,8 @@
+let p1 = [10, 20];
+let p2 = [5, 15];
+
+function midpoint(p1, p2) {
+    console.log("Midpoint m is: ",[(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]);
+};
+
+midpoint(p1, p2);
---
+++ Rotate_Number.js
@@ -0,0 +1,11 @@
+/* WAP to rotate number */
+
+let num = 917;
+
+function rotate(num) {
+    let rotation = [];
+    let str = Array.from(String(num));;
+    for (const element of str) {
+        
+    }
+}
